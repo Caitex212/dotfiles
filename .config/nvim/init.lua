56e698d27aebe6e2f00c84a6673aef3201390863
Caitex212 <firewizard787@gmail.com>
2026-02-22 23:54:26 +0100

    more nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,28 +1,116 @@
-vim.opt.termguicolors = true
-require("config.pckr")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require("pckr").add{
-
+require("lazy").setup({
   -- Theme
-  { "catppuccin/nvim", as = "catppuccin" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- LSP Management
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    build = ":MasonUpdate",
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      vim.lsp.config("pyright", {})
+
+      vim.lsp.config("clangd", {
+        capabilities = {
+          offsetEncoding = { "utf-16" },
+        },
+      })
+
+      vim.lsp.enable("pyright")
+      vim.lsp.enable("clangd")
+    end
+  },
 
   -- Treesitter
-  { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter'.setup {
+        ensure_installed = {'rust', 'javascript', 'python', 'java', 'cpp'},
+        highlight = { enable = true },
+      }
+    end
+  },
 
-  -- Telescope
-  { "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } },
+  -- Windsurf / Codeium
+  {
+    "Exafunction/windsurf.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp" },
+    config = function()
+      require("codeium").setup({
+        virtual_text = {
+          enabled = true,
+          key_bindings = {
+            accept = "<Tab>",
+            next = "<M-j>",
+            prev = "<m-h>",
+          }
+        }
+      })
+    end
+  },
 
-  -- Git
-  "lewis6991/gitsigns.nvim",
-}
+  -- PlatformIO
+  {
+    'anurag3301/nvim-platformio.lua',
+    dependencies = {
+      'akinsho/toggleterm.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+      'folke/which-key.nvim',
+    },
+    config = function()
+      vim.g.pioConfig = {
+        lsp = 'clangd',
+        clangd_source = 'ccls',
+        menu_key = '<leader>\\',
+      }
+      require('platformio').setup(vim.g.pioConfig)
+    end
+  },
 
-vim.cmd.colorscheme("catppuccin")
-
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua", "python", "javascript" },
-  highlight = { enable = true },
+  -- UI Elements
+  { 'akinsho/bufferline.nvim', dependencies = 'nvim-tree/nvim-web-devicons', config = true },
+  { 'nvim-lualine/lualine.nvim', opts = {} },
+  { 'onsails/lspkind.nvim' },
+  { 'lewis6991/gitsigns.nvim', opts = {} },
 })
 
-require("telescope").setup({})
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("catppuccin")
+vim.opt.clipboard = "unnamedplus"
 
-require("gitsigns").setup()
+local home = vim.fn.expand("~")  -- gets the user's home directory
+local xtensa_path = home .. "/.platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-*"
+vim.lsp.config("clangd", {
+  cmd = {
+    vim.fn.stdpath("data") .. "/mason/bin/clangd",
+    "--query-driver=" .. xtensa_path, -- This is not a good solution and does not even solve the issue completely
+  },
+})
+
+local cmp = require('cmp')
+cmp.setup({
+  sources = {{ name = "codeium" }},
+  formatting = {
+    format = require('lspkind').cmp_format({
+      mode = "symbol",
+      symbol_map = { Codeium = "" }
+    })
+  }
+})
---
+++ .config/nvim/lua/config/plugins/platformio.lua
@@ -0,0 +1,13 @@
+return {
+  'anurag3301/nvim-platformio.lua',
+
+  -- Dependencies are lazy-loaded by default unless specified otherwise.
+  dependencies = {
+    { 'akinsho/toggleterm.nvim' },
+    { 'nvim-telescope/telescope.nvim' },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    { 'nvim-lua/plenary.nvim' },
+    { 'folke/which-key.nvim' },
+    { 'nvim-treesitter/nvim-treesitter' }
+  },
+}
